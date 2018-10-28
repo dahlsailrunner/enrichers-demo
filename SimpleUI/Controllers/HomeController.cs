3f6a5689a8fb9f9a5506964a9360dd8919c8718a
Erik Dahl <dahlsailrunner@yahoo.com>
2018-10-28 16:17:09 -0500

    initial commit

---
+++ SimpleUI/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleUI.Models;
+
+namespace SimpleUI.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var x = User;
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SimpleUI/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using Serilog.Enrichers.AspnetcoreHttpcontext;
+using Microsoft.AspNetCore.Http;
+using SimpleUI.Models;
+using System.Linq;
+using Serilog.Core;
+
+namespace SimpleUI
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            //Log.Logger = new LoggerConfiguration()
+            //    .MinimumLevel.Debug()
+            //    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            //    .Enrich.FromLogContext()            
+            //    .WriteTo.Console(
+            //            outputTemplate: "{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}")
+            //    .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog((provider, ContextBoundObject, loggerConfig) =>
+                {
+                    loggerConfig
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                        .Enrich.WithAspnetcoreHttpcontext(provider, false, AddCustomContextInfo)
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console(
+                            outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {NewLine}{HttpContext} {NewLine}{UserInfo}");
+                }, true);
+
+        public static void AddCustomContextInfo(IHttpContextAccessor ctx, LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            HttpContext context = ctx.HttpContext;
+            if (context == null)
+            {
+                return;
+            }
+            var userInfo = context.Items[$"serilog-enrichers-aspnetcore-userinfo"] as UserInfo;
+            if (userInfo == null)
+            {
+                var user = context.User.Identity;
+                if (user == null || !user.IsAuthenticated) return;
+                userInfo = new UserInfo
+                {
+                    Name = user.Name,
+                    Claims = context.User.Claims.ToDictionary(x => x.Type, y => y.Value)
+                };
+                context.Items[$"serilog-enrichers-aspnetcore-userinfo"] = userInfo;
+            }
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserInfo", userInfo, true));
+        }
+    }
+}
